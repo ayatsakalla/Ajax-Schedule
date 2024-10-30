d44f87d6c229b9cc302ea4cec34c7f2dfcbced0d
ayatsakalla <525asakalla@frhsd.com>
2024-10-30 09:49:38 -0400

    added some java - onclick on a button stuff

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-$(document).ready(function () {
+const bellSchedule = {
+    1: { start: '8:24 AM', end: '9:31 AM' },
+    2: { start: '9:36 AM', end: '10:43 AM' },
+    3: { start: '10:48 AM', end: '11:55 AM' },
+    lunch: { start: '11:55 AM', end: '12:36 PM' },
+    4: { start: '12:41 PM', end: '1:48 PM' },
+    5: { start: '1:53 PM', end: '3:00 PM' }
+};
 
-    $('#submitDay').on('click', function () {
-        $.ajax({
-            url: `https://api.npoint.io/c865733db70b577f165c`,
-            method: 'GET',
-            success: function () {
+const letterDayPeriods = {
+    A: [1, 2, 3, "Lunch", 5, 6],
+    B: [4, 1, 2, "Lunch", 7, 5],
+    C: [3, 4, 1, "Lunch", 6, 6],
+    D: [2, 3, 4, "Lunch", 5, 6],
+    E: [1, 2, 3, "Lunch", 7, 5],
+    F: [4, 1, 2, "Lunch", 6, 7],
+    G: [3, 4, 7, "Lunch", 5, 6]
+};
 
-                pageCounter++
 
-            },
-            error: function () {
+$('#submitDay').on('click', function () {
+    $.ajax({
+        url: `https://api.npoint.io/c865733db70b577f165c`,
+        method: 'GET',
+        success: function () {
 
-            }
-        });
-    })
+            pageCounter++;
 
-});
+        },
+        error: function () {
+
+        }
+    });
+})
