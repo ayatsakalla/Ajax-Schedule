8417950c2a51ad2f9a4d05aa000ddae8e9c7b56b
ayatsakalla <525asakalla@frhsd.com>
2024-10-30 10:21:52 -0400

    oi boy

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const bellSchedule = {
-    1: { start: '8:24 AM', end: '9:31 AM' },
-    2: { start: '9:36 AM', end: '10:43 AM' },
-    3: { start: '10:48 AM', end: '11:55 AM' },
-    lunch: { start: '11:55 AM', end: '12:36 PM' },
-    4: { start: '12:41 PM', end: '1:48 PM' },
-    5: { start: '1:53 PM', end: '3:00 PM' }
-};
+$.ajax({
+    url: `https://api.npoint.io/c865733db70b577f165c`,
+    method: 'GET',
+    success: function () {
 
-const letterDayPeriods = {
-    A: [1, 2, 3, "Lunch", 5, 6],
-    B: [4, 1, 2, "Lunch", 7, 5],
-    C: [3, 4, 1, "Lunch", 6, 6],
-    D: [2, 3, 4, "Lunch", 5, 6],
-    E: [1, 2, 3, "Lunch", 7, 5],
-    F: [4, 1, 2, "Lunch", 6, 7],
-    G: [3, 4, 7, "Lunch", 5, 6]
-};
+    },
+    error: function () {
 
+    }
+});
+
+// const letterDayPeriods = {
+//     A: [1, 2, 3, "Lunch", 5, 6],
+//     B: [4, 1, 2, "Lunch", 7, 5],
+//     C: [3, 4, 1, "Lunch", 6, 6],
+//     D: [2, 3, 4, "Lunch", 5, 6],
+//     E: [1, 2, 3, "Lunch", 7, 5],
+//     F: [4, 1, 2, "Lunch", 6, 7],
+//     G: [3, 4, 7, "Lunch", 5, 6]
+// };
+
+$('.error').hide;
 
 $('#submitDay').on('click', function () {
-    $.ajax({
-        url: `https://api.npoint.io/c865733db70b577f165c`,
-        method: 'GET',
-        success: function () {
+    const textBox = $('#dayInput');
+    const letterDay = textBox.val().toUpperCase();
+    console.log(letterDay);
 
-            pageCounter++;
-
-        },
-        error: function () {
-
-        }
-    });
-})
+    if (letterDay != ("A" || "B" || "C" || "D" || "E" || "F" || "G")) {
+        letterDay == null;
+        alert("Invalid letter day!");
+        console.log("error");
+    }
+});
