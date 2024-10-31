54750ffa8c05141b3b38f68d264cbe173887fc82
ayatsakalla <525asakalla@frhsd.com>
2024-10-31 11:14:30 -0400

    my javascript works yay and i added my double lab

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-$.ajax({
-    url: `https://api.npoint.io/c865733db70b577f165c`,
-    method: 'GET',
-    success: function () {
+$('#submitDay').on("click", function () {
+    let selectedDay = $("#dayInput").val().trim().toUpperCase();
 
-    },
-    error: function () {
+    if (!["A", "B", "C", "D", "E", "F", "G"].includes(selectedDay)) {
+        alert("Invalid Letter Day");
+        return;
+    } else {
+        $.ajax({
+            url: "https://api.npoint.io/599c64f17cb3497dd77b",
+            method: "GET",
+            success: function (data) {
+                const schedule = data.schedule;
 
+                const letterDaySchedule = schedule.filter((obj) =>
+                    obj.days.includes(selectedDay)
+                );
+                renderHTML(letterDaySchedule);
+            },
+        });
     }
 });
 
-// const letterDayPeriods = {
-//     A: [1, 2, 3, "Lunch", 5, 6],
-//     B: [4, 1, 2, "Lunch", 7, 5],
-//     C: [3, 4, 1, "Lunch", 6, 6],
-//     D: [2, 3, 4, "Lunch", 5, 6],
-//     E: [1, 2, 3, "Lunch", 7, 5],
-//     F: [4, 1, 2, "Lunch", 6, 7],
-//     G: [3, 4, 7, "Lunch", 5, 6]
-// };
 
-$('.error').hide;
+function renderHTML(daySchedule) {
+    $("#scheduleList").empty();
+    let htmlTable = "";
+    daySchedule.forEach(function (classArray) {
+        htmlTable += `
+                    <tr>
+                    <td>${classArray.period}</td>
+                    <td>${classArray.class}</td>
+                    <td>${classArray.teacher}</td>
+                    <td>${classArray.room}</td>
+                   </tr> `;
+    });
 
-$('#submitDay').on('click', function () {
-    const textBox = $('#dayInput');
-    const letterDay = textBox.val().toUpperCase();
-    console.log(letterDay);
-
-    if (letterDay != ("A" || "B" || "C" || "D" || "E" || "F" || "G")) {
-        letterDay == null;
-        alert("Invalid letter day!");
-        console.log("error");
-    }
-});
+    $("#scheduleList").append(htmlTable);
+}
